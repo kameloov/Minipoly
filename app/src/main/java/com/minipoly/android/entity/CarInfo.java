f3d18781345340bd6f24554485c8848f577787ef
kamel <kamel.mohammad.mohammad@gmail.com>
2019-06-11 14:00:45 +0200

    more work

---
+++ app/src/main/java/com/minipoly/android/entity/CarInfo.java
@@ -0,0 +1,69 @@
+package com.minipoly.android.entity;
+
+import java.io.Serializable;
+
+public class CarInfo implements Serializable {
+    private String type;
+    private String brand;
+    private String color;
+    private int kilometres;
+    private boolean used;
+    private String fuel;
+    private boolean automatic;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getKilometres() {
+        return kilometres;
+    }
+
+    public void setKilometres(int kilometres) {
+        this.kilometres = kilometres;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public String getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    public boolean isAutomatic() {
+        return automatic;
+    }
+
+    public void setAutomatic(boolean automatic) {
+        this.automatic = automatic;
+    }
+}
